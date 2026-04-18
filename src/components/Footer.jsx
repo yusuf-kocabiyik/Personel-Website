e28764ad00b8dc82bef6c29f3458aea4378098d5
yusuf-kocabiyik <kocabiyikyusuf3@gmail.com>
2026-04-19 02:19:42 +0300

    küçük eklemeler

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = ()=>{
 
             <h1 className="text-center lg:text-left text-[36px] lg:text-[42px] w-full lg:w-120 font-semibold px-6 lg:px-2 dark:text-darkTitle1">{content.footer.text}</h1>
             <div className="font-medium text-lg flex flex-col gap-3 lg:gap-0 lg:flex-row justify-between px-2">
-                <span href="" className="text-center mx-auto lg:mx-0 text-[#AF0C48] border-b-2 border-[#AF0C48] dark:text-[#BAB2E7] dark:border-[#BAB2E7]">{content.footer.email}</span>
+                <a href="mailto:kocabiyikyusuf3@email.com?subject=Let's work together&body=Hi Yusuf," className="text-center mx-auto lg:mx-0 text-[#AF0C48] border-b-2 border-[#AF0C48] dark:text-[#BAB2E7] dark:border-[#BAB2E7]">{content.footer.email}</a>
                 <div className="flex mx-auto lg:mx-0 justify-around lg:justify-between w-70">
                     <a href="" className="dark:text-[#E1E1FF]">{content.footer.links[0]}</a>
-                    <a href="" className="text-[#00AB6B]">{content.footer.links[1]}</a>
-                    <a href="" className="text-[#0077B5]">{content.footer.links[2]}</a>
+                    <a href="https://github.com/yusuf-kocabiyik" className="text-[#00AB6B]">{content.footer.links[1]}</a>
+                    <a href="https://www.linkedin.com/in/yusufkocabiyik/" className="text-[#0077B5]">{content.footer.links[2]}</a>
                 </div>
             </div>
             <br /><br />
